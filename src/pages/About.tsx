f65144063c862d0b0d97fb6c4981718c7d9d46a2
Nikita <nikitakemper555@gmail.com>
2023-03-15 14:21:07 +0300

    feat: add pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About us</h1>
+        <p>This page tells you about us.</p>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/pages/Notfound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Notfound extends React.Component {
+  render() {
+    return (
+      <div>
+        This is page does not exist.
+      </div>
+    );
+  }
+}
+
+export default Notfound;
