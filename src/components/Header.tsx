aa90b5517ff868a0d4051f141c01995c4bab61f9
Nikita <nikitakemper555@gmail.com>
2023-03-16 00:37:25 +0300

    feat: add dynamic title(test)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,52 @@
-import '../styles/Header.scss';
+import './Header.scss';
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-class Header extends React.Component {
+export type THeaderProps = {
+  title: string;
+};
+
+enum Title {
+  HOME = 'Home',
+  ABOUT = 'About',
+  NOTFOUND = 'Not found',
+}
+
+class Header extends React.Component<THeaderProps> {
+  constructor(props: THeaderProps) {
+    super(props);
+  }
+
   render() {
     return (
       <header className="header">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <h3>{this.props.title}</h3>
+        <nav className="header-nav nav">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
       </header>
     );
   }
 }
 
-export default Header;
+function HeaderWithTitle() {
+  const location = useLocation();
+
+  let title;
+  switch (location.pathname) {
+    case '/':
+      title = Title.HOME;
+      break;
+    case '/about':
+      title = Title.ABOUT;
+      break;
+    default:
+      title = Title.NOTFOUND;
+  }
+
+  return <Header title={title} />;
+}
+
+export default HeaderWithTitle;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import Header from '../components/Header';
+import HeaderWithTitle from '../components/Header';
 
 class RootLayout extends React.Component {
   render() {
     return (
       <>
-        <Header />
+        <HeaderWithTitle />
         <Outlet />
       </>
     );
