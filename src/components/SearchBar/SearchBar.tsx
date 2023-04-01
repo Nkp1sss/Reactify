5860e5c71c9059c926af5372d13feae76afbef10
Nikita <nikitakemper555@gmail.com>
2023-04-01 22:57:28 +0300

    refactor: refactor code to react-hook-form

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -18,10 +18,6 @@ function SearchBar() {
     };
   }, []);
 
-  function changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    setInputValue(e.target.value);
-  }
-
   return (
     <div className="search__container">
       <p className="search__title">Go ahead, hover over search</p>
@@ -30,59 +26,10 @@ function SearchBar() {
         type="text"
         placeholder="Search"
         value={inputValue}
-        onChange={changeHandler}
+        onChange={(e) => setInputValue(e.target.value)}
       />
     </div>
   );
 }
 
 export default SearchBar;
-
-// class SearchBar extends React.Component<object, TSearchBarState> {
-//   constructor(props: object) {
-//     super(props);
-
-//     this.state = {
-//       inputValue: localStorage.getItem('savedValue') || '',
-//     };
-
-//     this.changeHandler = this.changeHandler.bind(this);
-//   }
-
-//   // componentDidMount(): void {
-//   //   const savedValue = localStorage.getItem('savedValue');
-
-//   //   if (savedValue) {
-//   //     this.setState({
-//   //       inputValue: savedValue,
-//   //     });
-//   //   }
-//   // }
-
-//   componentWillUnmount(): void {
-//     localStorage.setItem('savedValue', this.state.inputValue);
-//   }
-
-//   changeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-//     this.setState({
-//       inputValue: e.target.value,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="search__container">
-//         <p className="search__title">Go ahead, hover over search</p>
-//         <input
-//           className="search__input"
-//           type="text"
-//           placeholder="Search"
-//           value={this.state.inputValue}
-//           onChange={this.changeHandler}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default SearchBar;
