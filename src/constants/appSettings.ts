bd33be04ac2cfbd4d9aad2291c79066e16a42d11
Nikita <nikitakemper555@gmail.com>
2023-04-16 22:44:47 +0300

    feat: add API slice

--- src/constants/appSettings.ts
+++ src/constants/appSettings.ts
@@ -11,6 +11,6 @@ enum HeaderTitle {
   NOTFOUND = 'Not found',
 }
 
-const API_BASE_URL = 'https://json-server-6d972uqd4-nkp1sss.vercel.app';
+const API_BASE_URL = 'https://json-server-6d972uqd4-nkp1sss.vercel.app/';
 
 export { ROUTES, HeaderTitle, API_BASE_URL };
--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import SearchReducer from './slices/search';
 import FormCardsReducer from './slices/formCards';
+import CardsApi from './slices/cardsAPI';
 
 const store = configureStore({
   reducer: {
     search: SearchReducer,
     formCards: FormCardsReducer,
+    [CardsApi.reducerPath]: CardsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(CardsApi.middleware),
 });
 
 export default store;
