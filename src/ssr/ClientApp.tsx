a35b86ca83697153ab9956ea4673a398f7c85722
Nikita <nikitakemper555@gmail.com>
2023-04-20 18:42:43 +0300

    feat: add serverapp

--- src/ssr/ClientApp.tsx
+++ src/ssr/ClientApp.tsx
@@ -1,11 +1,11 @@
-import { hydrateRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { hydrateRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from "../components/App/App";
+import App from '../components/App/App';
 import store from '../redux';
 
 hydrateRoot(
-  document.getElementById("root") as Element,
+  document.getElementById('root') as Element,
   <Provider store={store}>
     <BrowserRouter>
       <App />
---
+++ src/ssr/ServerApp.tsx
@@ -0,0 +1,17 @@
+import { renderToPipeableStream, RenderToPipeableStreamOptions } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { Provider } from 'react-redux';
+import App from '../components/App/App';
+import store from '../redux';
+
+export default function render(url: string, opts: RenderToPipeableStreamOptions) {
+  const stream = renderToPipeableStream(
+    <Provider store={store}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </Provider>,
+    opts
+  );
+  return stream;
+}
