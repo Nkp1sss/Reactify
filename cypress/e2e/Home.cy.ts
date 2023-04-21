f5f19ebea5a84c69bd74346d780b66f05901307c
Nikita <nikitakemper555@gmail.com>
2023-04-21 23:57:23 +0300

    feat: add home tests

---
+++ cypress/e2e/Home.cy.ts
@@ -0,0 +1,14 @@
+describe('Home page', () => {
+  it('home visit', () => {
+    cy.visit('/');
+  });
+
+  it('card renders', () => {
+    cy.visit('/');
+    cy.request('https://json-server-6d972uqd4-nkp1sss.vercel.app/catalog').then(response => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.length(12);
+      cy.get('div.card').should('have.length', response.body.length);
+    });
+  });
+});
