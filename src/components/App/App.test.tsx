4d6af5fa32cae57c980830bc54e961be1f44c030
Nikita <nikitakemper555@gmail.com>
2023-04-21 14:23:15 +0300

    fix: fix tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,11 +1,16 @@
 import { screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { renderWithProviders } from '../../tests';
 import App from './App';
 
 describe('App', () => {
   it('Render App correctly', () => {
-    renderWithProviders(<App />);
+    renderWithProviders(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     const home = screen.getByText(/Go ahead, hover over search/);
     expect(home).toBeInTheDocument();
   });
