cf508cbe54bb55a827e1323ab1602f4888689edf
Nikita <nikitakemper555@gmail.com>
2023-05-01 16:46:46 +0300

    fix: fix lint errors

--- cypress/e2e/Home.cy.ts
+++ cypress/e2e/Home.cy.ts
@@ -5,7 +5,7 @@ describe('Home page', () => {
 
   it('card renders', () => {
     cy.visit('/');
-    cy.request('https://json-server-6d972uqd4-nkp1sss.vercel.app/catalog').then(response => {
+    cy.request('https://json-server-6d972uqd4-nkp1sss.vercel.app/catalog').then((response) => {
       expect(response.status).to.equal(200);
       expect(response.body).to.have.length(12);
       cy.get('div.card').should('have.length', response.body.length);
--- cypress/e2e/SearchBar.cy.ts
+++ cypress/e2e/SearchBar.cy.ts
@@ -1,7 +1,8 @@
 describe('Search Bar', () => {
   it('Search Bar works correctly', () => {
     cy.visit('/');
-    cy.get('input.search__input').type('search value').type('{enter}');
+    cy.get('input.search__input').type('search value');
+    cy.get('input.search__input').type('{enter}');
     cy.visit('/notfound');
   });
 });
